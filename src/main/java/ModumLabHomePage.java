8214bb1a7978b1627aaf9c9f8607b1fd0816777f
EduardLudwig <lyehdik@yandex.ru>
2021-05-21 18:12:12 +0300

    Initialize project

---
+++ src/main/java/ModumLabHomePage.java
@@ -0,0 +1,36 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+
+/**
+ *
+ * Class for
+ * @author E. Ludwig
+ *
+ */
+public class ModumLabHomePage {
+    private WebDriver driver;
+
+    public ModumLabHomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    private By cookiesNotificationEnable = By.xpath("//div[@class='notification js-fixed-margin -active']");
+    private By notificationClose = By.xpath("//div[@id='notification']/a");
+
+    /**
+     *
+     */
+    public void clickNotificationClose() {
+        driver.findElement(notificationClose).click();
+    }
+
+    public boolean isDisplayedCookieNotification() {
+        try {
+            driver.findElement(cookiesNotificationEnable);
+            return true;
+        } catch (NoSuchElementException ignore) {
+            return false;
+        }
+    }
+}
